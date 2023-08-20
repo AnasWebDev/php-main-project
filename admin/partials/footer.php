5648a2ab297a9a052756931c9c6a870ba6ba28cc
AnasWebDev <anasyasien101@gmail.com>
2023-08-21 00:16:26 +0300

    delete old project, start working in the new repo

--- admin/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer Section Starts -->
-<div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2023 All rights reserved, Food House. Developed By - <a href="#">Group6</a></p>
-        </div>
-    </div>
-    <!-- Footer Section Ends -->
-</body>
-</html>
